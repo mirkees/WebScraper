cfc16511430cb5b368cd5267a42667136621bb12
Markus Elmenreich <m.elmenreich@gmail.com>
2024-02-19 09:51:34 +0100

    v2 with blockchains

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const NodesGuruScraper = require("./nodesGuruScraper");
 const RootDataScraper = require("./rootDataScraper");
 
 
-//const nodesGuruScraper = new NodesGuruScraper();
-//nodesGuruScraper.nodesGuruScraper();
+const nodesGuruScraper = new NodesGuruScraper();
+nodesGuruScraper.nodesGuruScraper();
 
-const rootDataScraper = new RootDataScraper();
-rootDataScraper.rootDatascraper();
+//const rootDataScraper = new RootDataScraper();
+//rootDataScraper.rootDatascraper();
 
